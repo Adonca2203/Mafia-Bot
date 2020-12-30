51fc981180910d376420dd846aa0b9789def91e5
Lane Fuerstenberg <lane.fuerstenberg@protonmail.com>
2020-12-30 02:47:18 -0600

    Removed one sub directory that made project unable to run because imports were under a directory not expected

---
+++ PopulateGame.py
@@ -0,0 +1,51 @@
+
+from Roles import *
+import random
+
+#Default game mode will be:
+# Doctor (Support)
+# Investigator, Sheriff (Inno Invest)
+# Godfather, Mafioso (Mafia)
+# Serial Killer (Neutral Killer)
+# Vigilante and Jailor (Inno Killing)
+
+_testlist = ["Toshi"]
+
+all_roles = [Doctor, Investigator, Sheriff, Mafioso, Godfather, SerialKiller, Vigilante, Jailor]
+
+_test_all_roles = [Mafioso]
+
+roles_dict = {}
+
+def PopulateGame(players: list) -> dict:
+
+    for player in players:
+
+        _retrole = random.choice(all_roles)
+
+        playerchoice = _retrole(player)
+
+        all_roles.remove(_retrole)
+
+        roles_dict[player] = playerchoice
+
+    print(roles_dict)
+    print(all_roles)
+
+    return roles_dict
+
+def TestPopulateGame(players: list) -> dict:
+
+    for player in players:
+
+        _retrole = random.choice(_test_all_roles)
+
+        playerchoice = _retrole(player)
+
+        _test_all_roles.remove(_retrole)
+
+        roles_dict[player] = playerchoice
+
+    return roles_dict
+
+#TestPopulateGame(_testlist)
